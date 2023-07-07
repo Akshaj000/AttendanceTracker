67b49ee775890eb040a80238639798ec8dd5aee3
Akshaj Sr <akshajsr000@gmail.com>
2023-07-07 10:40:19 +0530

    fixes

--- client/session.js
+++ client/session.js
@@ -73,5 +73,5 @@ logoutButton.addEventListener('click', () => {
   localStorage.removeItem('refreshToken');
   access = null;
   refresh = null;
-  window.location.href = 'login.html';
+  window.location.href = 'index.html';
 });
